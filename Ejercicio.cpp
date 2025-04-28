6d90df0a24baae4625d0cc59429bcc0062b10696
Fernando18e6 <00137925@uca.edu.sv>
2025-04-28 08:24:51 -0600

    Add files via upload

---
+++ Ejercicio.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main(){
+//Definir edad
+ int age=0;
+
+ cout<<"welcome to the park";
+ cout<<"\nEnter your age to assign your price: ";
+ cin>>age;
+
+ if (age <= 5){
+    cout<<age<<"Your payment is 0";
+ }
+ else if (age >= 5 and age <= 12){
+    cout<<age<<"Your price to pay 5 Dollars";
+ }
+else if(age > 12){
+ cout<<age<<"Your price to pay 10 Dollars";
+}
+else{
+    cout<<"Put a valid format";
+}
+return 0; 
+}
