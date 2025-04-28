4e475fefcc16c27a0c8a27e4c56e3f481bf8c913
Fernando18e6 <00137925@uca.edu.sv>
2025-04-28 08:26:54 -0600

    Ejercicio.cpp

--- Ejercicio.cpp
+++ Ejercicio.cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main(){
 //Definir edad
  int age=0;
-
+//Pedir la edad
  cout<<"welcome to the park";
  cout<<"\nEnter your age to assign your price: ";
  cin>>age;
 
+ //Asignar el cobro basado en la edad
  if (age <= 5){
     cout<<age<<"Your payment is 0";
  }
